8aef5c8a81b6ea109ec3f6b4293dfe823ed0914d
gitBurning <974201360@qq.com>
2017-06-23 16:11:17 +0800

    Initial Commit

---
+++ CircleRoundAnmation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircleRoundAnmation
+//
+//  Created by gitBurning on 2017/6/23.
+//  Copyright © 2017年 BR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CircleRoundAnmation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CircleRoundAnmation
+//
+//  Created by gitBurning on 2017/6/23.
+//  Copyright © 2017年 BR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
